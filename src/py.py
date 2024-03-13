bf972edf96f35c6d35957c94e1971e290def86db
Volkinzz <134457219+Volkinzz@users.noreply.github.com>
2024-03-13 20:27:21 -0300

    Add files via upload

---
+++ src/py.py
@@ -0,0 +1,7 @@
+x = input('Deseja iniciar o programa? S/N\n').upper()
+
+while (x != "S" and x != "N"):
+    print("Digite S ou N, por favor!!!")
+    x = input('Deseja iniciar o programa? S/N\n').upper()
+
+print("Ufa, né")
