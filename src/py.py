70ebc4034d60b43a16b705d90ae7b1d6ca4c2319
Volkinzz <134457219+Volkinzz@users.noreply.github.com>
2024-03-14 07:04:18 -0300

    Delete src/py.py

--- src/py.py
+++
@@ -1,7 +0,0 @@
-x = input('Deseja iniciar o programa? S/N\n').upper()
-
-while (x != "S" and x != "N"):
-    print("Digite S ou N, por favor!!!")
-    x = input('Deseja iniciar o programa? S/N\n').upper()
-
-print("Ufa, né")
